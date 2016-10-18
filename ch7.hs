f5136f1b944aff45daee9318a07b60a5e7f18053
Soumya <soumya@soumyadsanyal.com>
2016-10-18 02:29:19 -0400

    .

--- ch7.hs
+++ ch7.hs
@@ -31,6 +31,27 @@ tensDigit :: Integral a => a -> a
 tensDigit = snd . f . fst . f
   where f = flip divMod 10
 
+hundredsDigit :: Integral a => a -> a 
+hundredsDigit = snd . g . f
+  where f = flip divMod 10
+        g = f . fst
+
+-- generalize the two exercises above
+--
+nthDigit :: Integral a => a -> Int -> a 
+nthDigit x n = snd $ ((iterate g) . f $ x) !! n
+  where f = flip divMod 10
+        g = f . fst
+
+foldBool :: a -> a -> Bool -> a
+foldBool x y c = case c of
+  True -> x
+  False -> y
+
+foldBool' :: a -> a -> Bool -> a
+foldBool' x y c 
+  | c = x
+  | True = y
 
 
 
