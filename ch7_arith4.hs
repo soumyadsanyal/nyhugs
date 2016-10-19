4c2f97504f3442ca2269755aefeb98566c7a49da
Soumya <soumya@soumyadsanyal.com>
2016-10-19 11:31:28 -0400

    .

--- ch7_arith4.hs
+++ ch7_arith4.hs
@@ -1,10 +1,14 @@
--- arith4.hs
 module Arith4 where
+-- arith4.hs
+
 -- id :: a -> a
 -- id x = x
 roundTrip :: (Show a, Read a) => a -> a 
-roundTrip a = read (show a)
+roundTrip a = read .show $ a
+
+
 main = do
-  print (roundTrip 4) print (id 4)
+  print (roundTrip 4) 
+  print (id 4)
 
 
